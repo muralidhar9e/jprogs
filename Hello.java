347ded5b21413d7e7de4e4c9928dbc9c9189a0f7
MURALI <muralidhar.9e@gmail.com>
2023-08-15 05:59:07 +0530

    added a java file with Hello.java

--- Hello.java
+++ Hello.java
@@ -4,5 +4,13 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hello");
+        int c = add(4,6);
+        System.out.println(c);
+        System.out.println("this is the message");
+
+    }
+
+    public int add(int a, int b){
+        return a+b;
     }
 }
